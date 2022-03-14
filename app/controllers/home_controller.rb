23b7a3ac6f22a1ad30f744c99cc69e2ac24638cc
askumardev <askumar.dev@outlook.com>
2022-03-14 09:41:46 +0530

    home controller update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ require 'open-uri'
 class HomeController < ApplicationController
   def index
     site = "https://www.bbc.co.uk/"
+    allowed_site = 'bbc.co'
     page = Nokogiri::HTML(URI.open(site))
     links = page.xpath('//a')
-    @links = links.map{ |element| element["href"]}
+    @links = links.map{ |element| element["href"] if element["href"].include? allowed_site}.compact
   end
 end
